4668d41309bc48f053468e4a4ee0783f1171d3d2
huhaomeng <emhhm@outlook.com>
2018-07-15 12:59:46 +0800

    merge fix

--- src/main/java/whustore/dao/OrderDao.java
+++ src/main/java/whustore/dao/OrderDao.java
@@ -128,15 +128,6 @@ public class OrderDao {
                 }
             }
         }
-        finally {
-            //关闭数据库连接
-            try {
-                if (conn != null)
-                    conn.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
         return true;
     }
 
@@ -164,15 +155,6 @@ public class OrderDao {
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
-            }
-        }
-        finally {
-            //关闭数据库连接
-            try {
-                if (conn != null)
-                    conn.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
             }
         }
         return true;
@@ -208,15 +190,6 @@ public class OrderDao {
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
-            }
-        }
-        finally {
-            //关闭数据库连接
-            try {
-                if (conn != null)
-                    conn.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
             }
         }
         System.out.println("获取订单ID失败");
